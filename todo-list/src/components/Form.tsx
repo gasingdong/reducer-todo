281d8dfa816f4ecb488a1f397d119c7cf93360ba
TheIllusiveC4 <gasingdong@gmail.com>
2019-08-13 13:12:42 -0700

    add form

---
+++ todo-list/src/components/Form.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Form = (): React.ReactElement => {
+  return (
+    <form>
+      <input type="text" />
+      <button type="submit">Add Todo</button>
+    </form>
+  );
+};
+
+export default Form;
